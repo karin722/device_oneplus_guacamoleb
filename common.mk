ellienieuwdorp/android_device_oneplus_sm8150-common

sm8150-common: Build libgptutils with Android.bp
This is required to build bootctrl.sdm845 with Android.bp.

Bug: 78793464
Test: make

Change-Id: Iff4f50ad363cc003d60a5380addd79749b547514

common.mk
@@ -146,7 +146,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.msmnile \
     libcutils \
-    libgptutils \
+    libgptutils.oneplus_sm8150.recovery \
     libz
 
 # Bluetooth

karin722/device_oneplus_guacamoleb

common.mk
@@ -146,7 +146,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.msmnile \
     libcutils \
-    libgptutils \
+    libgptutils.oneplus_msmnile.recovery \
     libz
 
 # Bluetooth